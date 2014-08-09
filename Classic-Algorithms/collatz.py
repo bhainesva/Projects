6106996f0e71b8a5ca7b974934113071a8447eb6
bhainesva <bhainesva@gmail.com>
2014-08-09 18:29:06 -0400

    Add memoization, timing, terrible command line args

--- Classic-Algorithms/collatz.py
+++ Classic-Algorithms/collatz.py
@@ -2,8 +2,23 @@
 #using the following process: If n is even, divide it by 2. If n is odd, 
 #multiply it by 3 and add 1.
 import sys
+import argparse
+import time
 
-#naive iterative implementation
+def timeit(method):
+
+    def timed(*args, **kw):
+        ts = time.clock()
+        result = method(*args, **kw)
+        te = time.clock()
+
+        print '%f sec' % \
+              (te-ts)
+        return result
+
+    return timed
+
+# iterative implementation
 def collatzNaive(n):
 	if n<1:
 		return -1
@@ -19,7 +34,7 @@ def collatzNaive(n):
 		count += 1
 	return count
 
-#naive recursive implementation
+# recursive implementation
 def collatzRecursive(n):
 	if n < 1:
 		return -1
@@ -30,5 +45,72 @@ def collatzRecursive(n):
 	else:
 		return 1 + collatzRecursive(n * 3 + 1)
 	
-print collatzNaive(int(sys.argv[1]))
-print collatzRecursive(int(sys.argv[1]))
+#calculates stopping times with memoization
+def stoppingTimes(n):
+	store = stoppingTimes.store
+	if n < 1:
+		return -1
+	if n == 1:
+		return 0
+	if n not in store:
+		if n % 2:
+			store[n] = 1 + stoppingTimes(n * 3 + 1)
+		else:
+			store[n] = 1 + stoppingTimes(n/2)
+	return store[n]
+stoppingTimes.store = {}
+
+#uses the benefits of memoization
+def calcAll(n):
+	for i in range(n+1):
+		stoppingTimes(i)
+	return stoppingTimes.store[n]
+
+def parse():
+	parser = argparse.ArgumentParser(description='Calculates collatz stopping times')
+	parser.add_argument("num", type=int,
+			    help="number to calculate stopping time for")
+	parser.add_argument("-t", "--time", action="store_true",
+			    help="set this option to time the results")
+	parser.add_argument("-r", "--recursive", action="store_true",
+			    help="set this option to use the recursive function")
+	parser.add_argument("-i", "--iterative", action="store_true",
+			    help="set this option to use the iterative function")
+	parser.add_argument("-m", "--memoization", action="store_true",
+			    help="set this option to use memoization")
+	args = parser.parse_args()
+	return [args.time, args.recursive, args.iterative, args.memoization, args.num]
+
+if __name__ == "__main__":
+	args = parse()
+	if not (args[0]):
+		if not (args[1] or args[2] or args[3]):
+			print collatzNaive(args[-1])
+		if (args[1]):
+			print "Recursive Result: ", collatzRecursive(args[-1])
+		if (args[2]):
+			print "Iterative Result: ", collatzNaive(args[-1])
+		if (args[3]):
+			print "Calculate All", calcAll(args[-1])
+	else:
+		if not (args[1] or args[2] or args[3]):
+			tb = time.clock()
+			tmp = collatzNaive(args[-1])
+			te = time.clock()
+			print "Iterative result :", tmp, " took ", (te-tb), " seconds."
+		if (args[1]):
+			tb = time.clock()
+			tmp = collatzNaive(args[-1])
+			te = time.clock()
+			print "Iterative result :", tmp, " took ", (te-tb), " seconds."
+		if (args[2]):
+			tb = time.clock()
+			tmp = collatzRecursive(args[-1])
+			te = time.clock()
+			print "Recursive result :", tmp, " took ", (te-tb), " seconds."
+		if (args[3]):
+			tb = time.clock()
+			tmp = calcAll(args[-1])
+			te = time.clock()
+			print "Memoization result :", tmp, " took ", (te-tb), " seconds."
+
